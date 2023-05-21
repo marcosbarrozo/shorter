6b8daa57dd0fa15cf8641932d3be20bc3fcb1143
Marcos Barrozo <marcos.barrozo18@gmail.comm>
2023-05-20 21:31:41 -0300

    swagger render link

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
       title: 'My API',
       description: 'Temple API'
     },
-    host: 'http://localhost:8080',
+    host: 'shorter-rvta.onrender.com',
     schemes: ['https']
   };
 
